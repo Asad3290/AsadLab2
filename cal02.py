64e6a9acb6dd2f9439cc1b5a2f4dcee6ed24fe2e
unknown <Administrator@ABCL3NI110.nc.com>
2021-09-28 17:04:56 +0500

    secondary

---
+++ cal02.py
@@ -0,0 +1,14 @@
+# Calculator in python v01
+
+def add(num1, num2):
+	return num1+num2
+
+def sub(num1, num2):
+	return num1-num2
+
+#main
+
+res = add(5, 3)
+res2 = sub(5,3)
+print(res)
+print(res2)
